cf62148b11933ac54556076285bb44552b4f0f84
Ibrawin <ibrahim-ayanbunmi@live.co.uk>
2023-12-06 13:58:15 +0000

    Add new find by methods into the Uom and Category repositories

--- src/main/java/com/ibrawin/recipeapp/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/ibrawin/recipeapp/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,9 @@ package com.ibrawin.recipeapp.repositories;
 import com.ibrawin.recipeapp.domain.UnitOfMeasure;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends JpaRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
