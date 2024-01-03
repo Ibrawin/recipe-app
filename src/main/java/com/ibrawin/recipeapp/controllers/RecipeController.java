1b92654e7c5cb9915ded7404769d840ab34d48e1
Ibrawin <ibrahim-ayanbunmi@live.co.uk>
2024-01-03 22:34:20 +0000

    Add get function to implement new recipes and add the post function to store the recipe

--- src/main/java/com/ibrawin/recipeapp/controllers/RecipeController.java
+++ src/main/java/com/ibrawin/recipeapp/controllers/RecipeController.java
@@ -1,15 +1,22 @@
 package com.ibrawin.recipeapp.controllers;
 
+import com.ibrawin.recipeapp.domain.Recipe;
+import com.ibrawin.recipeapp.dto.RecipeClientDTO;
+import com.ibrawin.recipeapp.dto.RecipeServerDTO;
 import com.ibrawin.recipeapp.service.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Slf4j
 @Controller
 public class RecipeController {
 
-    private RecipeService recipeService;
+    private final RecipeService recipeService;
 
     public RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
@@ -19,5 +26,17 @@ public class RecipeController {
     public String getRecipePage(Model model, @PathVariable Long id) {
         model.addAttribute("recipe", recipeService.getRecipeById(id));
         return "recipe/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeClientDTO());
+        return "recipe/recipe-form";
+    }
+
+    @RequestMapping(value = "recipe", method = RequestMethod.POST)
+    public String postRecipe(@ModelAttribute RecipeServerDTO recipeServerDTO) {
+        Recipe recipe = recipeService.addRecipe(recipeServerDTO);
+        return "redirect:/recipe/show/" + recipe.getId();
     }
 }
